76f84cb163b11d5f5d5c71f3391859f2e3cb2bbd
andrewbeng89 <madbeng@gmail.com>
2013-08-20 16:13:52 +0800

    updated ot-test: included two collaborative prototypes using fabricjs (using sharejs and socket.io)

--- app.js
+++ app.js
@@ -2,19 +2,19 @@
 /**
  * Module dependencies.
  */
+ var express = require('express')
+ , routes = require('./routes')
+ , user = require('./routes/user')
+ , http = require('http')
+ , path = require('path')
+ , connect = require('connect')
+ , sharejs = require('share').server;
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path')
-  , connect = require('connect');
+ var app = express(),
+ cookieParser = express.cookieParser('your secret here'),
+ sessionStore = new connect.middleware.session.MemoryStore();
 
-var app = express(),
-  cookieParser = express.cookieParser('your secret here'),
-  sessionStore = new connect.middleware.session.MemoryStore();
-
-app.configure(function(){
+ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -30,22 +30,22 @@ app.configure(function(){
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function(){
+ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', function(req, res) {
-	res.redirect('/index.html');
-});
-app.get('/users', user.list);
+ app.get('/', function(req, res) {
+   res.redirect('/index.html');
+ });
+ app.get('/users', user.list);
 
-var server = http.createServer(app),
-  io = require('socket.io').listen(server);
+ var server = http.createServer(app),
+ io = require('socket.io').listen(server);
 
-var SessionSockets = require('session.socket.io'),
-  sessionSockets = new SessionSockets(io, sessionStore, cookieParser);
+ var SessionSockets = require('session.socket.io'),
+ sessionSockets = new SessionSockets(io, sessionStore, cookieParser);
 
-io.enable('browser client minification');
+ io.enable('browser client minification');
 // send minified client
 io.enable('browser client etag');
 // apply etag caching logic based on version number
@@ -67,7 +67,26 @@ sessionSockets.on('connection', function(err, socket, session) {
       type: data.type
     });
   });
+
+  socket.on('drawCanvas', function(data) {
+    socket.broadcast.emit('drawData', data);
+  });
 });
+
+var options = {
+  db: {type: 'none'},
+  browserChannel: {cors: '*'},
+  auth: function(client, action) {
+    // This auth handler rejects any ops bound for docs starting with 'readonly'.
+    if (action.name === 'submit op' && action.docName.match(/^readonly/)) {
+      action.reject();
+    } else {
+      action.accept();
+    }
+  }
+};
+
+sharejs.attach(app, options);
 
 server.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
